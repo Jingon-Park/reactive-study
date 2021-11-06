61b5f23750b1b3dbd05526fd63cca5806405f826
Jingon Park <bennp3038@gmail.com>
2021-11-07 00:47:21 +0900

    Spring test

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,10 +1,40 @@
 package com.example.demo;
 
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.RestController;
 @SpringBootApplication
 public class Demo1Application {
+
+
+    @RestController
+    public static class Controller{
+
+        @RequestMapping("/hello")
+        public Publisher<String> hello(String name){
+            return new Publisher<String>(){
+                @Override
+                public void subscribe(Subscriber<? super String> sub){
+                    sub.onSubscribe(new Subscription() {
+                        @Override
+                        public void request(long n){
+                            sub.onNext("hello " + name);
+                            sub.onComplete();
+                        }
+
+                        @Override
+                        public void cancel(){
+                            
+                        }
+                    }); 
+                }
+            };
+        }
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);

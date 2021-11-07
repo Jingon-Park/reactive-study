aefe7a1b9f437012183c636883a74c30b32df5e3
Jingon Park <bennp3038@gmail.com>
2021-11-07 23:08:20 +0900

    scheduler Ex:

--- src/main/java/com/example/demo/reactive/DelegeateSub.java
+++ src/main/java/com/example/demo/reactive/DelegeateSub.java
@@ -1,32 +1,32 @@
-// package com.example.demo.reactive;
+package com.example.demo.reactive;
 
-// import org.reactivestreams.Subscriber;
-// import org.reactivestreams.Subscription;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
-// public class DelegeateSub<T, R> implements Subscriber<T>{
+public class DelegeateSub<T, R> implements Subscriber<T>{
 
-//     Subscriber sub;
-//     public DelegeateSub(Subscriber<? super R> sub) {
-//         this.sub = sub;
+    Subscriber sub;
+    public DelegeateSub(Subscriber<? super R> sub) {
+        this.sub = sub;
         
-//     }
+    }
     
-//     @Override
-//     public void onSubscribe(Subscription s) {
-//         sub.onSubscribe(s);
-//     }
-//     @Override
-//     public void onNext(T i) {
-//         sub.onNext(i);
-//     }
-//     @Override
-//     public void onError(Throwable throwable) {
-//         sub.onError(throwable);
-//     }
-//     @Override
-//     public void onComplete() {
-//         sub.onComplete();
-//     }
+    @Override
+    public void onSubscribe(Subscription s) {
+        sub.onSubscribe(s);
+    }
+    @Override
+    public void onNext(T i) {
+        sub.onNext(i);
+    }
+    @Override
+    public void onError(Throwable throwable) {
+        sub.onError(throwable);
+    }
+    @Override
+    public void onComplete() {
+        sub.onComplete();
+    }
 
     
-// }
+}
---
+++ src/main/java/com/example/demo/reactive/SchedulerEx.java
@@ -0,0 +1,117 @@
+package com.example.demo.reactive;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import ch.qos.logback.core.util.ExecutorServiceUtil;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SchedulerEx {
+
+    /*
+    이런 코드는 main 스레드에서 모든 코드들이 직렬적으로 실행된다.
+    위와 같은 코든느 핵심 스레드가 블로킹하는 방식으로 동작한다.
+    이런 방식은 효율이 떨어진다. 실제는 퍼블리셔와 서브스크라이버를 같은 스레드에서 작성하지 않는다.
+    scheduler를 통해서 위 문제를 해결할 수 있음.
+    */
+
+    public static void main(String[] args) {
+        Publisher<Integer> pub = sub -> {
+            sub.onSubscribe(new Subscription() {
+
+                @Override
+                public void request(long n) {
+                    // TODO Auto-generated method stub
+                    log.debug("request");
+                    sub.onNext(1);
+                    sub.onNext(2);
+                    sub.onNext(3);
+                    sub.onComplete();
+
+
+                    
+                }
+
+                @Override
+                public void cancel() {
+                    // TODO Auto-generated method stub
+                    
+                }
+                
+            });
+        };
+
+        Publisher<Integer> subOnPub = sub ->{
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            es.execute(() -> pub.subscribe(sub));
+        };
+
+        Publisher<Integer> pubOnSeb = sub ->{
+            pub.subscribe(new Subscriber<Integer>() {
+
+                ExecutorService ex = Executors.newSingleThreadExecutor();
+                @Override
+                public void onSubscribe(Subscription s) {
+                    // TODO Auto-generated method stub
+                    sub.onSubscribe(s);
+                    
+                }
+
+                @Override
+                public void onNext(Integer t) {
+                    // TODO Auto-generated method stub
+                    ex.execute(()-> sub.onNext(t));
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    // TODO Auto-generated method stub
+                    ex.execute(()-> sub.onError(t));
+                    
+                }
+
+                @Override
+                public void onComplete() {
+                    // TODO Auto-generated method stub
+                    ex.execute(()-> sub.onComplete());
+                }
+                
+            });
+        };
+
+        Subscriber<Integer> sub = new Subscriber<Integer>() {
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                // TODO Auto-generated method stub
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer t) {
+                // TODO Auto-generated method stub
+                log.debug("onNext : {}", t);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // TODO Auto-generated method stub
+                
+            }
+
+            @Override
+            public void onComplete() {
+                // TODO Auto-generated method stub
+                log.debug("onComplete");
+            }
+            
+        };
+        pubOnSeb.subscribe(sub);
+        log.debug("exit");
+    }
+}

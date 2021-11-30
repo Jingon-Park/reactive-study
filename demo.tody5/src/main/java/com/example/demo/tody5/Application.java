d475e7d83944f0baccd861378d58e4b4e72b08f3
Park Jin Gon <bennp3038@gmail.com>
2021-11-30 23:33:14 +0900

    시스템 환경 변수 충돌로 인해서 분ë¦

--- demo.tody5/src/main/java/com/example/demo/tody5/Application.java
+++ demo.tody5/src/main/java/com/example/demo/tody5/Application.java
@@ -18,25 +18,37 @@ public class Application {
 
 
 	
-	@RestController
-	public class MyController{
+//	@RestController
+//	public class MyController{
+//
+//		//RestTemplate rt = new RestTemplate();
+//		AsyncRestTemplate rt = new AsyncRestTemplate();
+//
+//		/**
+//		 * 이전 강의 4강에서 배운 내용인 Callable이나 DeferredResult의 경우
+//		 * DefferedResult는 외부에서 어떤 이벤트가 발생해야만하고
+//		 * Callable은 workerThread가 계속해서 생성되는 문제가 발생한다.
+//		 * 이를 Spring 3.0에서는 해결하지 못했다.
+//		 * 4.0에서 부터는 AsyncRestTemplate를 제공한다.
+//		 * 하지만 AsyncRestTemplate 또한 외부 API를 호출할 때 비동기로 동작하긴 하지만
+//		 * 비동기 처리를 위해서 백그라운드에 Thread를 추가적으로 만들기 때문에 서버에는 부하임
+//		 */
+//		@GetMapping("/rest")
+//		public ListenableFuture<ResponseEntity<String>> rest(int idx) {
+//			log.info("Application Start");
+//			return rt.getForEntity("http://localhost:8081/service?req={req}", String.class, "hello" + idx);
+//		}
+//	}
 
-		//RestTemplate rt = new RestTemplate();
+	@RestController
+	public static class MainController {
+		// asynchronous
 		AsyncRestTemplate rt = new AsyncRestTemplate();
 
-		/**
-		 * 이전 강의 4강에서 배운 내용인 Callable이나 DeferredResult의 경우
-		 * DefferedResult는 외부에서 어떤 이벤트가 발생해야만하고
-		 * Callable은 workerThread가 계속해서 생성되는 문제가 발생한다.
-		 * 이를 Spring 3.0에서는 해결하지 못했다.
-		 * 4.0에서 부터는 AsyncRestTemplate를 제공한다.
-		 * 하지만 AsyncRestTemplate 또한 외부 API를 호출할 때 비동기로 동작하긴 하지만
-		 * 비동기 처리를 위해서 백그라운드에 Thread를 추가적으로 만들기 때문에 서버에는 부하임
-		 */
 		@GetMapping("/rest")
 		public ListenableFuture<ResponseEntity<String>> rest(int idx) {
-			log.info("Application Start");
-			return rt.getForEntity("http://localhost:8081/service?req={req}", String.class, "hello" + idx);
+			return rt.getForEntity("http://localhost:8081/service?req={req}",
+					String.class, "hello" + idx);
 		}
 	}
 
@@ -44,6 +56,8 @@ public class Application {
 	
 
 	public static void main(String[] args) {
+		System.setProperty("server.port", "8080");
+		System.setProperty("server.tomcat.max-threads", "1");
 		SpringApplication.run(Application.class, args);
 	}
 
--- demo.tody5/src/main/java/com/example/demo/tody5/RemoteService.java
+++ demo.tody5/src/main/java/com/example/demo/tody5/RemoteService.java
@@ -1,25 +1,33 @@
 package com.example.demo.tody5;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@Slf4j
 public class RemoteService {
-    @RestController
-    public static class MyController {
 
+    @RestController
+    public static class RemoteController {
         @GetMapping("/service")
-        public String service1(String req) throws InterruptedException {
+        public String service(String req) throws InterruptedException {
+
+            log.info("called");
+            System.out.println(System.getProperty("server.tomcat.max-threads"));
+
             Thread.sleep(2000);
             return req + "/service";
         }
-        
     }
+
     public static void main(String[] args) {
-        System.setProperty("SERVER.PORT","8081");
-        System.setProperty("server.tomcat.max.threads","1000");
+        // 하나의 프로젝트에서 2개의 스프링 애플리케이션을 띄우기 위해 외부 서비스 역할을 하는 RemoteApplication은
+        // application.properties가 아닌 별도의 프로퍼티를 이용하도록 직접 설정한다.
+        System.setProperty("server.port", "8081");
+        System.setProperty("server.tomcat.max-threads", "1000");
+
         SpringApplication.run(RemoteService.class, args);
     }
 }
-

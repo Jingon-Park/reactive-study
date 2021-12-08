449156444b4cc2d6d687ebb6f89fc3211f21d919
Park Jin Gon <bennp3038@gmail.com>
2021-12-08 15:00:22 +0900

    7강 CompletableFuture 예제

--- tobi7/tobi7/src/main/java/com/example/tobi7/CFuture.java
+++ tobi7/tobi7/src/main/java/com/example/tobi7/CFuture.java
@@ -1,15 +1,41 @@
 package com.example.tobi7;
 
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Future는 비동기 작업의 결과를 담고있는 객체
  */
+
+@Slf4j
 public class CFuture {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
+        CompletableFuture
+                .supplyAsync(() -> {
+                    log.info("runAsync");
+                    return 1;
+                })
+                .thenApply(s -> {
+                    log.info("then apply {}", s);
+                    return s + 1;
+                })
+                .thenApply(s2 -> {
+                    log.info("then apply {}", s2);
+                    return s2 * 3;
+                })
+                .thenAccept(s3 -> {
+                    log.info("then accecpt {}", s3);
+                });
 
+        log.info("exit");
 
+        ForkJoinPool.commonPool().shutdown();
+        ForkJoinPool.commonPool().awaitTermination(10, TimeUnit.SECONDS);
     }
 }

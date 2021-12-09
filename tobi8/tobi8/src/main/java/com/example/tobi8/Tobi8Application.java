1434c1d9b18f4e5add45d76033de9137d3fa4022
Park Jin Gon <bennp3038@gmail.com>
2021-12-09 14:42:13 +0900

    토비 8강 init

---
+++ tobi8/tobi8/src/main/java/com/example/tobi8/Tobi8Application.java
@@ -0,0 +1,13 @@
+package com.example.tobi8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tobi8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tobi8Application.class, args);
+	}
+
+}
---
+++ tobi8/tobi8/src/test/java/com/example/tobi8/Tobi8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tobi8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

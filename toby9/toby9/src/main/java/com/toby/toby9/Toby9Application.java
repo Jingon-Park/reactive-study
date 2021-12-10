add47a57be02b784808d2b9b91c594e26d082eae
Park Jin Gon <bennp3038@gmail.com>
2021-12-10 17:24:49 +0900

    9강 init

---
+++ toby9/toby9/src/main/java/com/toby/toby9/Toby9Application.java
@@ -0,0 +1,13 @@
+package com.toby.toby9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toby9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toby9Application.class, args);
+	}
+
+}
---
+++ toby9/toby9/src/test/java/com/toby/toby9/Toby9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toby.toby9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toby9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

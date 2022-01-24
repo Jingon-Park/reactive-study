0a8d71e806892f68598dc5f1adfd1ce5dc2e9fce
Park Jin Gon <bennp3038@gmail.com>
2022-01-24 17:45:54 +0900

    test

--- toby10/toby10/src/main/java/com/example/toby10/Toby10Application.java
+++ toby10/toby10/src/main/java/com/example/toby10/Toby10Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Toby10Application {
 
+	//주석 추가
 	public static void main(String[] args) {
 		SpringApplication.run(Toby10Application.class, args);
 	}
